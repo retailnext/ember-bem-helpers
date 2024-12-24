da1637702ac8ad800a7154e149db41fa779a62bd
Andrey Fel <andrey.fel@retailnext.net>
2024-12-24 14:40:10 +0100

    Migrate to ember-qunit v9
    Explicitly invoke `loadTests()` and `setupEmberOnerrorValidation()`
    
    https://github.com/emberjs/ember-qunit/pull/1182

--- test-app/tests/test-helper.ts
+++ test-app/tests/test-helper.ts
@@ -3,7 +3,11 @@ import config from 'test-app/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
+
+setupEmberOnerrorValidation();
+loadTests();
 
 setApplication(Application.create(config.APP));
 
