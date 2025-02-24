063e47418ed364bc68cc10df578e5dc2832b41c2
Moises Ramirez <moises@retailnext.net>
2025-02-24 11:39:15 -0800

    Upgrade eslint configs to v9 "flat"
    For this, I created a new addon using the latest ember-cli and copied
    the generated `eslint.config.mjs` content and used that to replace our
    own config. From there, I removed unused settings and comments for
    example qunit, typescript and *.{gjs,gts} support in
    `ember-bem-helpers/`.

--- ember-bem-helpers/eslint.config.mjs
+++ ember-bem-helpers/eslint.config.mjs
@@ -1,44 +1,83 @@
-'use strict';
+import globals from 'globals';
+import js from '@eslint/js';
+import ember from 'eslint-plugin-ember/recommended';
+import prettier from 'eslint-plugin-prettier/recommended';
+import n from 'eslint-plugin-n';
+import babelParser from '@babel/eslint-parser';
 
-module.exports = {
-  root: true,
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    babelOptions: {
-      root: __dirname,
+const parserOptions = {
+  esm: {
+    js: {
+      ecmaFeatures: { modules: true },
+      ecmaVersion: 'latest',
+      requireConfigFile: false,
     },
   },
-  plugins: ['ember'],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:prettier/recommended',
-  ],
-  env: {
-    browser: true,
-  },
-  rules: {},
-  overrides: [
-    // node files
-    {
-      files: [
-        './.eslintrc.cjs',
-        './.prettierrc.cjs',
-        './.template-lintrc.cjs',
-        './addon-main.cjs',
-        './src/lib/**/*.js',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-      env: {
-        browser: false,
-        node: true,
-      },
-      plugins: ['n'],
-      extends: ['plugin:n/recommended'],
-    },
-  ],
 };
+
+export default [
+  js.configs.recommended,
+  ember.configs.base,
+  prettier,
+  {
+    ignores: ['dist/', 'node_modules/', 'coverage/', '!**/.*', '**/*.d.ts'],
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      parser: babelParser,
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      parserOptions: parserOptions.esm.js,
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: [
+      '**/*.cjs',
+      'config/**/*.js',
+      'testem.js',
+      'testem*.js',
+      '.prettierrc.js',
+      '.stylelintrc.js',
+      '.template-lintrc.js',
+      'ember-cli-build.js',
+    ],
+    plugins: {
+      n,
+    },
+
+    languageOptions: {
+      sourceType: 'script',
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.mjs'],
+    plugins: {
+      n,
+    },
+
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      parserOptions: parserOptions.esm.js,
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];
--- test-app/eslint.config.mjs
+++ test-app/eslint.config.mjs
@@ -1,82 +1,116 @@
-'use strict';
+import globals from 'globals';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import ember from 'eslint-plugin-ember/recommended';
+import prettier from 'eslint-plugin-prettier/recommended';
+import qunit from 'eslint-plugin-qunit';
+import n from 'eslint-plugin-n';
+import babelParser from '@babel/eslint-parser';
 
-module.exports = {
-  root: true,
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    requireConfigFile: false,
-    babelOptions: {
-      plugins: [
-        ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
-      ],
+const parserOptions = {
+  esm: {
+    js: {
+      ecmaFeatures: { modules: true },
+      ecmaVersion: 'latest',
+      requireConfigFile: false,
+      babelOptions: {
+        plugins: [
+          [
+            '@babel/plugin-proposal-decorators',
+            { decoratorsBeforeExport: true },
+          ],
+        ],
+      },
+    },
+    ts: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-  plugins: ['ember'],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:prettier/recommended',
-  ],
-  env: {
-    browser: true,
-  },
-  rules: {},
-  overrides: [
-    {
-      files: ['**/*.ts'],
-      extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-      ],
-      rules: {},
-    },
-    // node files
-    {
-      files: [
-        './.eslintrc.js',
-        './.prettierrc.js',
-        './.stylelintrc.js',
-        './.template-lintrc.js',
-        './ember-cli-build.js',
-        './testem.js',
-        './blueprints/*/index.js',
-        './config/**/*.js',
-        './lib/*/index.js',
-        './server/**/*.js',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-      env: {
-        browser: false,
-        node: true,
-      },
-      extends: ['plugin:n/recommended'],
-    },
-    {
-      files: ['**/*.gts'],
-      parser: 'ember-eslint-parser',
-      plugins: ['ember'],
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:ember/recommended',
-        'plugin:ember/recommended-gts',
-      ],
-    },
-    {
-      files: ['tests/**/*.{ts,gts}'],
-      rules: {
-        // override / enable optional rules
-        'ember/no-replace-test-comments': 'error',
-      },
-    },
-    {
-      // test files
-      files: ['tests/**/*-test.{ts,gts}'],
-      extends: ['plugin:qunit/recommended'],
-    },
-  ],
 };
+
+export default ts.config(
+  js.configs.recommended,
+  ember.configs.base,
+  ember.configs.gjs,
+  ember.configs.gts,
+  prettier,
+  {
+    ignores: ['dist/', 'node_modules/', 'coverage/', '!**/.*'],
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      parser: babelParser,
+    },
+  },
+  {
+    files: ['**/*.{js,gjs}'],
+    languageOptions: {
+      parserOptions: parserOptions.esm.js,
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ['**/*.{ts,gts}'],
+    languageOptions: {
+      parser: ember.parser,
+      parserOptions: parserOptions.esm.ts,
+    },
+    extends: [...ts.configs.recommendedTypeChecked, ember.configs.gts],
+  },
+  {
+    files: ['tests/**/*-test.{js,gjs,ts,gts}'],
+    plugins: {
+      qunit,
+    },
+    rules: {
+      'ember/no-replace-test-comments': 'error',
+    },
+  },
+  {
+    files: [
+      '**/*.cjs',
+      'config/**/*.js',
+      'testem.js',
+      'testem*.js',
+      '.prettierrc.js',
+      '.stylelintrc.js',
+      '.template-lintrc.js',
+      'ember-cli-build.js',
+    ],
+    plugins: {
+      n,
+    },
+
+    languageOptions: {
+      sourceType: 'script',
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.mjs'],
+    plugins: {
+      n,
+    },
+
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      parserOptions: parserOptions.esm.js,
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+);
