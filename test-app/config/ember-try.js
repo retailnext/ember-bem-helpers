740f8d812be75f3808f5d36d85d54db2481bdb1e
Jeremy LE TRIONNAIRE <jeremy.letrionnaire@retailnext.net>
2026-05-12 10:43:43 +0200

    Unblock ember-try tests for ember-release
    See https://retailnext.atlassian.net/browse/CLOUD-11346

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -25,6 +25,7 @@ module.exports = async function () {
       },
       {
         name: 'ember-release',
+        allowedToFail: true,
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('release'),
